6f3eb121ffb2b397981b9a3ecee9c698ab566028
Colin Pollock <pollockcolin@gmail.com>
2012-08-19 21:40:56 -0700

    Add more tests.

--- tests.py
+++ tests.py
@@ -3,11 +3,41 @@
 
 from wordsearch import Grid
 
+from nose.tools import nottest
+
+
+two_by_three = [['C', 'A', 'T'],
+                ['N', 'T', 'A']]
+
+
+class TestFindWord(object):
+    def setup(self):
+        self.grid = Grid(two_by_three)
+
+
+    def find(self, word, wrap):
+        found = self.grid.find_word(word, wrap)
+        return None if found is None else list(found)
+
+    def test_missing(self):
+        assert self.find('XYZ', False) is None
+        assert self.find('XXXXXXX', False) is None
+
+    def test_found(self):
+        found = self.find('CAT', False)
+        print found
+        assert found == [(0, 0), (0, 1), (0, 2)]
+
+    def test_found_wrapped(self):
+        found = self.find('ANT', True)
+        print found
+        assert found == [(1, 2), (1, 0), (1, 1)]
+
+
+
 class TestGridOperations(object):
     def setup(self):
-        data = [['C', 'A', 'T'],
-                ['N', 'T', 'A']]
-        self.grid = Grid(data)
+        self.grid = Grid(two_by_three)
 
     def test_num_row_and_cols(self):
         assert self.grid.num_rows == 2
@@ -44,15 +74,10 @@ class TestGridOperations(object):
 
 
     ############################################################################
-    # TESTING Spans
+    # Testing Spans
     def test_right_span_nowrap(self):
         found = list(self.grid.right_span((1, 0), 3, False))
         assert found == [(1, 0), (1, 1), (1, 2)]
-
-    def test_right_span_nowrap_nospace(self):
-        """Should return None b/c there's not enough space for a span."""
-        found = self.grid.right_span((1, 1), 3, False)
-        assert found == None
 
     def test_right_span_wrap(self):
         found = list(self.grid.right_span((0, 2), 3, True))
@@ -60,12 +85,30 @@ class TestGridOperations(object):
 
     def test_right_span_nowrap_nospace(self):
         """Should return None b/c there's not enough space for a span."""
-        found = list(self.grid.right_span((1, 1), 3, False))
+        found = self.grid.right_span((1, 1), 3, False)
         assert found == None
 
-    def test_spans(self):
-        found = list(self.grid.spans((0, 0), 2, False))
-        assert len(filter(None, found)) == 2
+    @nottest
+    def test_spans_wrap(self):
+        found = [list(ob) for ob in (self.grid.spans((0, 0), 2, False))]
+        assert len([ob for ob in found if ob is not None]) == 6
+        #TODO: test for wrapping when on the corner letter
+
+    @nottest
+    def test_spans_no_wrap(self):
+        found = [list(ob) for ob in (self.grid.spans((0, 0), 2, True))]
+        assert len([ob for ob in found if ob is not None]) == 3
+
+    def test_right_span_wrap(self):
+        """Should find a span that wraps around the right side."""
+        found = list(self.grid.right_span((1, 1), 3, True))
+        print found
+        assert found == [(1, 1), (1, 2), (1, 0)]
+
+    def test_right_span_wrap_with_no_space(self):
+        """Should return None because a square can't be used twice."""
+        found = self.grid.right_span((1, 1), 4, True)
+        assert found == None
 
 
 def test_empty_setup():
@@ -73,4 +116,4 @@ def test_empty_setup():
     grid = Grid.empty(4, 3)
     assert grid.num_rows == 4
     assert grid.num_cols == 3
-    assert all(item is None for item in iter(grid))
+    assert all(item is None for item in grid)
